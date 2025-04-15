4e501a00bfbd8d302e7f63480fa9bcf6f30bec8c
PriyaJ31 <priyankajawalkar2020@gmail.com>
2025-04-15 18:06:14 -0400

    70 percent done project

---
+++ src/assets/texts/skill.js
@@ -0,0 +1,32 @@
+export const skills = [
+    // Programming
+    { skill_name: "Java", img: "Java" },
+    { skill_name: "Python", img: "Python" },
+    { skill_name: "C++", img: "C++" },
+    { skill_name: "C", img: "C" },
+    { skill_name: "SQL", img: "SQL" },
+    { skill_name: "JavaScript", img: "JavaScript" },
+    { skill_name: "Typescript", img: "Typescript" },
+    { skill_name: "Haskell", img: "Haskell" },
+    { skill_name: "HTML", img: "HTML" },
+    { skill_name: "CSS", img: "CSS" },
+  
+    // Databases
+    { skill_name: "MySQL", img: "MySQL" },
+    { skill_name: "Microsoft SQL Server", img: "Microsoft SQL Server" },
+    { skill_name: "MongoDB", img: "MongoDB" },
+  
+    // Tools & Frameworks
+    { skill_name: "Node.js", img: "Node.js" },
+    { skill_name: "React.js", img: "React.js" },
+    { skill_name: "Svelte", img: "Svelte" },
+    { skill_name: "Angular", img: "Angular" },
+    { skill_name: "Bootstrap", img: "Bootstrap" },
+    { skill_name: "Git", img: "Git" },
+    { skill_name: "AWS", img: "AWS" },
+    { skill_name: "FastAPI", img: "FastAPI" },
+    { skill_name: "RestAPI", img: "RestAPI" },
+    { skill_name: "Jest", img: "Jest" },
+    { skill_name: "Postman", img: "Postman" },
+  ];
+  
